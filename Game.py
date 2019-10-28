ebb6f7aca3e2dbdcacd5f2218230986b6d237a67
Khaliullin Bulat <incidentalname636@gmail.com>
2019-10-29 01:15:52 +0300

    Добавил меню, изменения большие на всякий залью в новую ветку от мастера

--- Game.py
+++ Game.py
@@ -11,6 +11,7 @@ colors_by_value = {2: (248, 24, 148), 4: (237, 41, 57), 8: (255, 211, 0), 16: (2
 circles = []
 level = 1
 points = 0
+is_menu = True
 
 
 def get_value(limit):
@@ -92,114 +93,192 @@ class Circle:
         screen.blit(value, [self.x - 15, self.y - 8])
 
 
-screen.fill((255, 255, 255))
-x = 110
-y = 210
-f = get_value(1000)
-for i in range(5):
-    for j in range(5):
-        circle = Circle(x, y, next(f))
-        circle.draw(screen)
-        circles.append(circle)
-        x += 70
-    x = 110
-    y += 70
-draw_progress_bar()
-pygame.draw.rect(screen, (44, 117, 255), (360, 50, 80, 40))
-text3 = font.render('restart', True, (255, 255, 255))
-screen.blit(text3, [368, 60])
-pygame.display.update()
-
 gameOver = False
-previous_value = 0
-previous_center = (0, 0)
-last_circle_coors = (0, 0)
-sum = 0
 while not gameOver:
     pygame.time.delay(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameOver = True
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            coors = event.pos
-            if 360 <= coors[0] <= 440 and 50 <= coors[1] <= 90:
-                screen.fill((255, 255, 255))
-                previous_value = 0
-                previous_center = (0, 0)
-                last_circle_coors = (0, 0)
-                sum = 0
-                circles = []
-                level = 1
-                points = 0
-                x = 110
-                y = 210
-                f = get_value(1000)
-                for i in range(5):
-                    for j in range(5):
-                        circle = Circle(x, y, next(f))
-                        circle.draw(screen)
-                        value = font.render(str(circle.value), True, (255, 255, 255))
-                        screen.blit(value, [circle.x - 15, circle.y - 8])
-                        circles.append(circle)
-                        x += 70
-                    x = 110
-                    y += 70
+        if is_menu:
+            screen.fill((255, 255, 255))
+            pygame.draw.rect(screen, (44, 117, 255), (175, 200, 150, 40))
+            play_text = font.render('Play', True, (255, 255, 255))
+            screen.blit(play_text, [225, 210])
+            pygame.display.update()
+            pygame.draw.rect(screen, (44, 117, 255), (175, 280, 150, 40))
+            restart_text2 = font.render('Restart', True, (255, 255, 255))
+            screen.blit(restart_text2, [215, 290])
+            pygame.display.update()
+            pygame.draw.rect(screen, (44, 117, 255), (175, 360, 150, 40))
+            records_text = font.render('Records', True, (255, 255, 255))
+            screen.blit(records_text, [210, 370])
+            pygame.display.update()
 
-                #points_percent = float(points) / float(1000)
-                pygame.draw.rect(screen, (44, 117, 255), (100, 120, 1, 10))
-                text1 = font.render(str(level), True, (44, 117, 255))
-                text2 = font.render(str(level + 1), True, (44, 117, 255))
-                screen.blit(text1, [80, 115])
-                screen.blit(text2, [420, 115])
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                coors = event.pos
+                if 175 <= coors[0] <= 325 and 280 <= coors[1] <= 320:
+                    is_menu = False
+                    level = 0
+                    points = 0
+                    screen.fill((255, 255, 255))
+                    x = 110
+                    y = 210
+                    f = get_value(1000)
+                    circles = []
+                    for i in range(5):
+                        for j in range(5):
+                            circle = Circle(x, y, next(f))
+                            circle.draw(screen)
+                            circles.append(circle)
+                            x += 70
+                        x = 110
+                        y += 70
+                    else:
+                        for circle in circles:
+                            circle.draw(screen)
+
+                    draw_progress_bar()
+                    pygame.draw.rect(screen, (44, 117, 255), (360, 50, 80, 40))
+                    restart_text = font.render('restart', True, (255, 255, 255))
+                    screen.blit(restart_text, [368, 60])
+                    pygame.display.update()
+                    pygame.draw.rect(screen, (44, 117, 255), (80, 50, 80, 40))
+                    menu_text = font.render('menu', True, (255, 255, 255))
+                    screen.blit(menu_text, [94, 60])
+                    pygame.display.update()
+
+                    previous_value = 0
+                    previous_center = (0, 0)
+                    last_circle_coors = (0, 0)
+                    sum = 0
+                if 175 <= coors[0] <= 325 and 200 <= coors[1] <= 240:
+                    is_menu = False
+                    screen.fill((255, 255, 255))
+                    x = 110
+                    y = 210
+                    f = get_value(1000)
+                    if len(circles) == 0:
+                        for i in range(5):
+                            for j in range(5):
+                                circle = Circle(x, y, next(f))
+                                circle.draw(screen)
+                                circles.append(circle)
+                                x += 70
+                            x = 110
+                            y += 70
+                    else:
+                        for circle in circles:
+                            circle.draw(screen)
+
+                    draw_progress_bar()
+                    pygame.draw.rect(screen, (44, 117, 255), (360, 50, 80, 40))
+                    restart_text = font.render('restart', True, (255, 255, 255))
+                    screen.blit(restart_text, [368, 60])
+                    pygame.display.update()
+                    pygame.draw.rect(screen, (44, 117, 255), (80, 50, 80, 40))
+                    menu_text = font.render('menu', True, (255, 255, 255))
+                    screen.blit(menu_text, [94, 60])
+                    pygame.display.update()
+
+                    previous_value = 0
+                    previous_center = (0, 0)
+                    last_circle_coors = (0, 0)
+                    sum = 0
+        else:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                coors = event.pos
 
                 pygame.draw.rect(screen, (44, 117, 255), (360, 50, 80, 40))
-                text3 = font.render('restart', True, (255, 255, 255))
-                screen.blit(text3, [368, 60])
+                restart_text = font.render('restart', True, (255, 255, 255))
+                screen.blit(restart_text, [368, 60])
+                pygame.draw.rect(screen, (44, 117, 255), (80, 50, 80, 40))
+                menu_text = font.render('menu', True, (255, 255, 255))
+                screen.blit(menu_text, [94, 60])
 
-                pygame.display.update()
-            for circle in circles:
-                if (coors[0] - circle.x)**2 + (coors[1] - circle.y)**2 <= 30**2:
-                    if previous_value == 0 and previous_center == (0, 0):
-                        previous_value = circle.value
-                        previous_center = (circle.x, circle.y)
-                        last_circle_coors = (circle.x, circle.y)
-                        sum += circle.value
-                        circle.pressed = True
-                    else:
-                        if circle.value == previous_value and math.sqrt((circle.x - previous_center[0])**2 + (circle.y - previous_center[1])**2) <= 99:
-                            pygame.draw.line(screen, colors_by_value[circle.value], [previous_center[0], previous_center[1]], [circle.x, circle.y], 3)
-                            pygame.display.update()
+                if 360 <= coors[0] <= 440 and 50 <= coors[1] <= 90:
+                    level = 1
+                    points = 0
+                    screen.fill((255, 255, 255))
+                    x = 110
+                    y = 210
+                    f = get_value(1000)
+                    circles = []
+                    for i in range(5):
+                        for j in range(5):
+                            circle = Circle(x, y, next(f))
+                            circle.draw(screen)
+                            circles.append(circle)
+                            x += 70
+                        x = 110
+                        y += 70
+                    draw_progress_bar()
+                    pygame.draw.rect(screen, (44, 117, 255), (360, 50, 80, 40))
+                    restart_text = font.render('restart', True, (255, 255, 255))
+                    screen.blit(restart_text, [368, 60])
+                    pygame.display.update()
+                    pygame.draw.rect(screen, (44, 117, 255), (80, 50, 80, 40))
+                    menu_text = font.render('menu', True, (255, 255, 255))
+                    screen.blit(menu_text, [94, 60])
+                    pygame.display.update()
+
+                    previous_value = 0
+                    previous_center = (0, 0)
+                    last_circle_coors = (0, 0)
+                    sum = 0
+
+                if 80 <= coors[0] <= 160 and 50 <= coors[1] <= 90:
+                    is_menu = True
+
+                for circle in circles:
+                    if (coors[0] - circle.x) ** 2 + (coors[1] - circle.y) ** 2 <= 30 ** 2:
+                        if previous_value == 0 and previous_center == (0, 0):
                             previous_value = circle.value
                             previous_center = (circle.x, circle.y)
                             last_circle_coors = (circle.x, circle.y)
                             sum += circle.value
                             circle.pressed = True
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
-            screen.fill((255, 255, 255))
-            pygame.draw.rect(screen, (44, 117, 255), (360, 50, 80, 40))
-            text3 = font.render('restart', True, (255, 255, 255))
-            screen.blit(text3, [368, 60])
-            for circle in circles:
-                if circle.pressed:
-                    if (circle.x, circle.y) != last_circle_coors:
-                        new_circle = Circle(circle.x, circle.y, next(f))
-                        circles.remove(circle)
-                        circles.append(new_circle)
-                        new_circle.draw(screen)
+                        else:
+                            if circle.value == previous_value and math.sqrt(
+                                    (circle.x - previous_center[0]) ** 2 + (circle.y - previous_center[1]) ** 2) <= 99:
+                                pygame.draw.line(screen, colors_by_value[circle.value],
+                                                 [previous_center[0], previous_center[1]], [circle.x, circle.y], 3)
+                                pygame.display.update()
+                                previous_value = circle.value
+                                previous_center = (circle.x, circle.y)
+                                last_circle_coors = (circle.x, circle.y)
+                                sum += circle.value
+                                circle.pressed = True
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                screen.fill((255, 255, 255))
+                pygame.draw.rect(screen, (44, 117, 255), (360, 50, 80, 40))
+                restart_text = font.render('restart', True, (255, 255, 255))
+                screen.blit(restart_text, [368, 60])
+                pygame.display.update()
+                pygame.draw.rect(screen, (44, 117, 255), (80, 50, 80, 40))
+                menu_text = font.render('menu', True, (255, 255, 255))
+                screen.blit(menu_text, [94, 60])
+                pygame.display.update()
+                for circle in circles:
+                    if circle.pressed:
+                        if (circle.x, circle.y) != last_circle_coors:
+                            new_circle = Circle(circle.x, circle.y, next(f))
+                            circles.remove(circle)
+                            circles.append(new_circle)
+                            new_circle.draw(screen)
+                        else:
+                            new_circle = Circle(circle.x, circle.y, choise_sum(sum))
+                            circles.remove(circle)
+                            circles.append(new_circle)
+                            new_circle.draw(screen)
                     else:
-                        new_circle = Circle(circle.x, circle.y, choise_sum(sum))
-                        circles.remove(circle)
-                        circles.append(new_circle)
-                        new_circle.draw(screen)
-                else:
-                    circle.draw(screen)
-            points += sum * get_coefficient()
-            previous_center = (0, 0)
-            previous_value = 0
-            last_circle_coors = (0, 0)
-            sum = 0
-            check_level()
-            draw_progress_bar()
-            pygame.display.update()
+                        circle.draw(screen)
+                points += sum * get_coefficient()
+                previous_center = (0, 0)
+                previous_value = 0
+                last_circle_coors = (0, 0)
+                sum = 0
+                check_level()
+                draw_progress_bar()
+                pygame.display.update()
     pygame.display.update()
 pygame.quit()
